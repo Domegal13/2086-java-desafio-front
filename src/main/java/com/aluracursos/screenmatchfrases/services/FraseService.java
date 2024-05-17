4392f726e2385bc520339ca8bda49c1a6e992562
Domegal13 <Domegal13cursos@gmail.com>
2024-05-17 20:17:20 -0300

    first commit

---
+++ src/main/java/com/aluracursos/screenmatchfrases/services/FraseService.java
@@ -0,0 +1,20 @@
+package com.aluracursos.screenmatchfrases.services;
+
+import com.aluracursos.screenmatchfrases.dto.FraseDTO;
+import com.aluracursos.screenmatchfrases.model.Frase;
+import com.aluracursos.screenmatchfrases.repository.FraseRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FraseService {
+
+    @Autowired
+    private FraseRepository repository;
+    public FraseDTO obtenerFraseAleatoria() {
+
+        Frase frase = repository.obtenerFraseAleatoria();
+        return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonaje(), frase.getPoster());
+    }
+}
